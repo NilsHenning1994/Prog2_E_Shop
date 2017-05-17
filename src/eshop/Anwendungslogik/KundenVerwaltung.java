0294a29da3cb49649d3f1845c1ee2203490fac6f
Nils Henning <nils.henning94@gmail.com>
2017-05-17 16:40:59 +0200

    Kundenverwaltung: Exceptions + Code Kommentare

--- src/eshop/Anwendungslogik/KundenVerwaltung.java
+++ src/eshop/Anwendungslogik/KundenVerwaltung.java
@@ -6,12 +6,15 @@ import java.util.List;
 import eshop.Datenstrukturen.Adresse;
 import eshop.Datenstrukturen.Benutzer;
 import eshop.Datenstrukturen.Kunde;
+import eshop.Exceptions.BenutzerExistiertBereitsException;
+import eshop.Exceptions.EinloggenFehlgeschlagenException;
 
 public class KundenVerwaltung {
 
 	private List<Kunde> kundenliste = new ArrayList<Kunde>();
 	
-	public Kunde einloggen(String mail, String passwort){
+	// Kunde wird eingeloggt mittels E-Mail und Passwort
+	public Kunde einloggen(String mail, String passwort) throws EinloggenFehlgeschlagenException{
 		for(int i = 0; i< kundenliste.size();i++){
 			Kunde ku = kundenliste.get(i);
 			if(ku.getEmail().equals(mail) && ku.getPasswort().equals(passwort)){
@@ -19,28 +22,30 @@ public class KundenVerwaltung {
 				return ku;
 			}
 			 
-		}return null; // TODO: besser LoginFailedException
+		} throw new EinloggenFehlgeschlagenException(); 
 	}
 
 	
+	// Kunde wird ausgeloggt
 	public void ausloggen(Benutzer kunde){
 		kunde.setLogin(false);
 	}
 	
 	
-	
-	public boolean registrieren(String vorname, String nachname, String mail, String passwort, Adresse adresse){
+	// Kunde wird registriert mittels Vorname, Nachname, E-Mail, Passwort und Adresse
+	public boolean registrieren(String vorname, String nachname, String mail, String passwort, Adresse adresse) throws BenutzerExistiertBereitsException{
 		
 		int id = kundenliste.size()+1;
 		Kunde ku = new Kunde(id, vorname, nachname, mail, adresse, passwort, false);
 		if(!kundenliste.contains(ku)){
 			kundenliste.add(ku);
 			return true;
+		} else{
+			throw new BenutzerExistiertBereitsException(ku); 
 		}
-		return false;	// TODO: besser UserAlreadyExistsException
 	}
 
-
+	
 	public List<Kunde> getKundenliste() {
 		return kundenliste;
 	}
