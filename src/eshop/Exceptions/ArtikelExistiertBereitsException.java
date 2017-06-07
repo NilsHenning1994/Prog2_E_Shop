3aff73ff47cb41801e54123c77b7745f8e9341fb
DESKTOP-MPABITH\Befreak <christoph.helweg@gmail.com>
2017-06-07 21:16:44 +0200

    h

--- src/eshop/Exceptions/ArtikelExistiertBereitsException.java
+++ src/eshop/Exceptions/ArtikelExistiertBereitsException.java
@@ -1,6 +1,9 @@
 package eshop.Exceptions;
 
 import java.rmi.RemoteException;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import eshop.Datenstrukturen.Artikel;
 
@@ -22,4 +25,17 @@ public class ArtikelExistiertBereitsException extends Exception {
 		super("Artikel mit Titel " + artikel.getBez() + " und Artikelnr " + artikel.getNummer()
 				+ " existiert bereits. " + zusatzMsg);
 	}
+	
+	// Create an instance of SimpleDateFormat used for formatting 
+	// the string representation of date (month/day/year)
+	DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+
+	// Get the date today using Calendar object.
+	Date today = Calendar.getInstance().getTime();        
+	// Using DateFormat format method we can create a string 
+	// representation of a date with the defined format.
+	String reportDate = df.format(today);
+
+	// Print what date is today!
+	System.out.println("Report Date: " + reportDate);
 }
